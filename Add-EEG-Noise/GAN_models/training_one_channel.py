1d573f7c5c9386e6a55cbf76f80433206bfeabca
ZLFan <2224057402@qq.com>
2021-06-17 15:27:27 +0800

    correct-bug

--- Add-EEG-Noise/GAN_models/training_one_channel.py
+++ Add-EEG-Noise/GAN_models/training_one_channel.py
@@ -189,9 +189,9 @@ for epoch in range(Number_epochs):
             X_noise = get_noise()
             noise = X_noise[idx]
             noise = np.squeeze(noise, axis=2).astype(np.float32)
-
+            eegnoise=noise[1:120]
             #noise = np.random.normal(0, 1, (batch_size, 120)).astype(np.float32)
-            discriminator_loss.append(discriminator_model.train_on_batch([image_batch,noise, y_value],
+            discriminator_loss.append(discriminator_model.train_on_batch([image_batch,eegnoise, y_value],
                                                                          [positive_y, negative_y, dummy_y]))
 
         sampled_labels = np.random.randint(0, 2, batch_size).reshape(-1, 1)
